3724347096351677586cec3cc14f06bc545fb0a2
Alharbi-code <hamni20021@hotmail.com>
2025-11-01 22:08:06 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        arabic: ['Tajawal', 'Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
