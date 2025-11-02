0d330248e2c4dcd44488de1bb023ec39492fc6a9
Alharbi-code <hamni20021@hotmail.com>
2025-11-02 22:48:42 +0300

    Create marketData.js

---
+++ src/data/marketData.js
@@ -0,0 +1,7 @@
+export const marketTrends = [
+  { major: "الأمن السيبراني", demand: 92, avgSalary: 1250 },
+  { major: "علوم الحاسب", demand: 88, avgSalary: 1150 },
+  { major: "الهندسة المدنية", demand: 76, avgSalary: 1050 },
+  { major: "الذكاء الاصطناعي", demand: 95, avgSalary: 1300 },
+  { major: "التسويق الرقمي", demand: 70, avgSalary: 800 }
+]
