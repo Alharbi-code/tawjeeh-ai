5a3afa2fb9f6a56f4a3c985261c76b4e745dbec2
Alharbi-code <hamni20021@hotmail.com>
2025-11-04 18:14:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// نقطة الدخول الرئيسية للتطبيق
-// Entry point for Tawjeeh AI application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
